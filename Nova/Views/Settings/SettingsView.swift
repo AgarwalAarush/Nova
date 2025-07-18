6c27070f4d51f56cac4f8c3602b51e4244d35cc5
Aarush Agarwal <aarushaga@gmail.com>
2025-07-17 22:52:16 -0400

    fixed ai api key view

--- Nova/Views/Settings/SettingsView.swift
+++ Nova/Views/Settings/SettingsView.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e600a9494e61cb03598654e2177ccfbc2b83670b8f643cc17a5e52e6676161f
-size 45791
+oid sha256:b0ed39253f11f77116ce2d42d5699690974e86c9b2d5b0c22bd5d02b5d7432cf
+size 44751
