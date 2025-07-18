a7aea93fbb8e6af55a46691e0205a1cca0bf6583
Aarush Agarwal <aarushaga@gmail.com>
2025-07-17 23:11:31 -0400

    final update for jul 17

--- Nova/Views/Settings/SettingsView.swift
+++ Nova/Views/Settings/SettingsView.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0ed39253f11f77116ce2d42d5699690974e86c9b2d5b0c22bd5d02b5d7432cf
-size 44751
+oid sha256:c98fb7220c71d60917cbfe079f5f28f9c16f8131d1ca5f31bda0f50e0ff71a24
+size 45228
